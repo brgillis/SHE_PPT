dd5712c8efaa4ceaa0b45be2f4508a0d3522cdda
Malte Tewes <mtewes@astro.uni-bonn.de>
2017-08-18 14:15:59 +0200

    Fixing import issue in test_she_image

--- SHE_PPT/tests/python/test_she_image.py
+++ SHE_PPT/tests/python/test_she_image.py
@@ -1,21 +1,47 @@
-"""This script gives a small demo of the image object.
+#
+# Copyright (C) 2012-2020 Euclid Science Ground Segment
+#
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 3.0 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#"""This script gives a small demo of the image object.
+
+"""
+File: tests/python/test_she_image.py
+
+Created on: 08/18/17
+Author: user
 """
 
-import she_image
+from __future__ import division, print_function
+from future_builtins import *
+
+import pytest
+import SHE_PPT.she_image
+
 import numpy as np
 
-
+"""
 size = 50
-
-
-
 array = np.random.randn(size**2).reshape((size, size))
-
 mask = np.zeros(size**2).reshape((size, size))
-
 a = she_image.SHEImage(array, mask)
+"""
+
+class Test_she_image():
+
+    def test_test(self):
+        assert True
 
 
 
-
-print(a)
