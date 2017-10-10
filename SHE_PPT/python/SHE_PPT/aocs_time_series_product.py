c29ecaa0014224f4f1c3cdcced02539449abdf79
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-10-10 16:09:01 +0100

    Adding AOCS time series product class

---
+++ SHE_PPT/python/SHE_PPT/aocs_time_series_product.py
@@ -0,0 +1,86 @@
+""" @file aocs_time_series_product.py
+
+    Created 10 Oct 2017
+
+    Functions to create and output an aocs_time_series data product.
+
+    ---------------------------------------------------------------------
+
+    Copyright (C) 2012-2020 Euclid Science Ground Segment      
+       
+    This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General    
+    Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)    
+    any later version.    
+       
+    This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied    
+    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more    
+    details.    
+       
+    You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to    
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+# import HeaderProvider.GenericHeaderProvider as HeaderProvider # FIXME
+# import EuclidDmBindings.she.she_stub as she_dpd # FIXME
+
+import pickle
+
+def init():
+    """
+        Adds some extra functionality to the DpdSheAstrometry product
+    """
+    
+    # binding_class = she_dpd.DpdSheAocsTimeSeriesProduct # @FIXME
+    binding_class = DpdSheAocsTimeSeriesProduct
+
+    # Add needed methods here
+    pass
+
+class DpdSheAocsTimeSeriesProduct: # @FIXME
+    def __init__(self):
+        self.Header = None
+        self.Data = None
+    def validateBinding(self):
+        return False
+        
+class SheAocsTimeSeriesProduct: # @FIXME
+    def __init__(self):
+        pass
+
+def create_dpd_she_aocs_time_series():
+    """
+        @TODO fill in docstring
+    """
+    
+    # dpd_she_aocs_time_series = she_dpd.DpdSheAocsTimeSeriesProduct() # @FIXME
+    dpd_she_aocs_time_series = DpdSheAocsTimeSeriesProduct()
+    
+    # dpd_she_aocs_time_series.Header = HeaderProvider.createGenericHeader("SHE") # FIXME
+    dpd_she_aocs_time_series.Header = "SHE"
+    
+    dpd_she_aocs_time_series.Data = create_she_aocs_time_series()
+    
+    return dpd_she_aocs_time_series
+
+def create_she_aocs_time_series():
+    """
+        @TODO fill in docstring
+    """
+    
+    # she_aocs_time_series = she_dpd.SheAocsTimeSeriesProduct() # @FIXME
+    she_aocs_time_series = SheAocsTimeSeriesProduct()
+    
+    return she_aocs_time_series
