5dc50b53ccba5a6cc9b9549b76afbd037b83d95a
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-11-16 17:22:16 +0000

    Fixing zero-indexing

--- SHE_PPT/python/SHE_PPT/details_table_format.py
+++ SHE_PPT/python/SHE_PPT/details_table_format.py
@@ -240,8 +240,8 @@ def initialise_details_table(image = None,
     
     if detector is not None:
         logger.warn("'detector' argument for initialise_*_table is deprecated: Use detector_x and detector_y instead.")
-        detector_x = detector % 6
-        detector_y = detector // 6
+        detector_x = detector % 6 + 1
+        detector_y = detector // 6 + 1
     
     if optional_columns is None:
         optional_columns = [tf.psf_x,tf.psf_y]
@@ -268,8 +268,8 @@ def initialise_details_table(image = None,
         # Get values from the image object, unless they were passed explicitly
         
         if detector_x or detector_y is None:
-            detector_x = image.get_local_ID() % 6
-            detector_y = image.get_local_ID() // 6
+            detector_x = image.get_local_ID() % 6 + 1
+            detector_y = image.get_local_ID() // 6 + 1
         
         if subtracted_sky_level is None:
             subtracted_sky_level = image.get_param_value('subtracted_background')
