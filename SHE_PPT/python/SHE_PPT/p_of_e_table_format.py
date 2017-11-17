28ec36319a019dc5c0410f2006a5cd4a3610380b
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-11-17 18:45:58 +0000

    Defining P(e) table format

---
+++ SHE_PPT/python/SHE_PPT/p_of_e_table_format.py
@@ -0,0 +1,160 @@
+""" @file p_of_e_table_format.py
+
+    Created 10 Oct 2017
+
+    Format definition for galaxy population table.
+"""
+
+# Copyright (C) 2012-2020 Euclid Science Ground Segment      
+#        
+# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General    
+# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)    
+# any later version.    
+#        
+# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied    
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more    
+# details.    
+#        
+# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to    
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+from collections import OrderedDict
+
+from astropy.table import Table
+
+from SHE_PPT.utility import hash_any
+
+class POfETableMeta(object):
+    """
+        @brief A class defining the metadata for PSF tables.
+    """
+    
+    def __init__(self):
+        
+        self.__version__ = "0.1.0"
+        self.table_format = "she.pofeTable"
+        
+        # Table metadata labels
+        self.version = "SS_VER"
+        self.format = "SS_FMT"
+        
+        # Store the less-used comments in a dict
+        self.comments = OrderedDict(((self.version, None),
+                                     (self.format, None),
+                                   ))
+        
+        # A list of columns in the desired order
+        self.all = self.comments.keys()
+
+class POfETableFormat(object):
+    """
+        @brief A class defining the format for galaxy population priors tables. Only the p_of_e_table_format
+               instance of this should generally be accessed, and it should not be changed.
+    """
+    
+    def __init__(self):
+        
+        # Get the metadata (contained within its own class)
+        self.meta = POfETableMeta()
+        
+        # And a quick alias for it
+        self.m = self.meta
+        
+        # Get the version from the meta class
+        self.__version__ = self.m.__version__
+        
+        # Direct alias for a tuple of all metadata
+        self.meta_data = self.m.all
+        
+        # Dicts for less-used properties
+        self.is_optional = OrderedDict()
+        self.comments = OrderedDict()
+        self.dtypes = OrderedDict()
+        self.fits_dtypes = OrderedDict()
+        self.lengths = OrderedDict()
+        
+        def set_column_properties( name, is_optional=False, comment=None, dtype=">f4", fits_dtype="E",
+                                   length=1):
+            
+            assert name not in self.is_optional
+            
+            self.is_optional[name] = is_optional
+            self.comments[name] = comment
+            self.dtypes[name] = dtype
+            self.fits_dtypes[name] = fits_dtype
+            self.lengths[name] = length
+            
+            return name
+        
+        # Column names and info
+        
+        self.ID = set_column_properties("ID", dtype=">i8", fits_dtype="K", comment="Link to galaxy population table.")
+        
+        self.e1 = set_column_properties("E1", comment="Using flat weight function.")
+        self.e2 = set_column_properties("E2", comment="Using flat weight function.")
+        
+        self.bulge_e1 = set_column_properties("BULGE_E1")
+        self.bulge_e2 = set_column_properties("BULGE_E2")
+        
+        self.disk_e1 = set_column_properties("BULGE_E1")
+        self.disk_e2 = set_column_properties("BULGE_E2")
+        
+        # A list of columns in the desired order
+        self.all = self.is_optional.keys()
+        
+        # A list of required columns in the desired order
+        self.all_required = []
+        for label in self.all:
+            if not self.is_optional[label]:
+                self.all_required.append(label)
+
+# Define an instance of this object that can be imported         
+p_of_e_table_format = POfETableFormat()
+
+# And a convient alias for it
+tf = p_of_e_table_format
+
+
+def make_p_of_e_table_header():
+    """
+        @brief Generate a header for a galaxy population table.
+        
+        @return header <OrderedDict>
+    """
+    
+    header = OrderedDict()
+    
+    header[tf.m.version] = tf.__version__
+    header[tf.m.format] = tf.m.table_format
+    
+    return header
+
+def initialise_p_of_e_table(optional_columns = None):
+    """
+        @brief Initialise a galaxy population table.
+        
+        @return p_of_e_table <astropy.Table>
+    """
+    
+    if optional_columns is None:
+        optional_columns = []
+    else:
+        # Check all optional columns are valid
+        for colname in optional_columns:
+            if colname not in tf.all:
+                raise ValueError("Invalid optional column name: " + colname)
+    
+    names = []
+    init_cols = []
+    dtypes = []
+    for colname in tf.all:
+        if (colname in tf.all_required) or (colname in optional_columns):
+            names.append(colname)
+            init_cols.append([])
+            dtypes.append((tf.dtypes[colname],tf.lengths[colname]))
+    
+    p_of_e_table = Table(init_cols, names=names, dtype=dtypes)
+    
+    p_of_e_table.meta = make_p_of_e_table_header()
+    
+    return p_of_e_table
