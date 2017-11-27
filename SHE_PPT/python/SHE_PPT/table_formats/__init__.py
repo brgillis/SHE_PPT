9351de2de687473a9334ee7ad1b567cac63769a8
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-11-27 18:13:30 +0000

    Automating imports

--- SHE_PPT/python/SHE_PPT/table_formats/__init__.py
+++ SHE_PPT/python/SHE_PPT/table_formats/__init__.py
@@ -20,3 +20,7 @@ from os.path import dirname, basename, isfile
 import glob
 modules = glob.glob(dirname(__file__)+"/*.py")
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+
+del modules, dirname, basename, isfile, f, glob
+
+from . import *
