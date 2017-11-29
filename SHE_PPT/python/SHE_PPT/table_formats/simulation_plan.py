64df53c51fec367c034102f5ea5d7c8d4f7ec865
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-11-29 16:16:43 +0000

    Adding simulation plan table

---
+++ SHE_PPT/python/SHE_PPT/table_formats/simulation_plan.py
@@ -0,0 +1,169 @@
+""" @file simulation_plan_table_format.py
+
+    Created 29 Nov 2017
+
+    Format definition for a table outlining a simulation plan.
+"""
+
+# Copyright (C) 2012-2020 Euclid Science Ground Segment      
+#        
+# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General    
+# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)    
+# any later version.    
+#        
+# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied    
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more    
+# details.    
+#        
+# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to    
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+from collections import OrderedDict
+
+from astropy.table import Table
+
+class SimulationPlanTableMeta(object):
+    """
+        @brief A class defining the metadata for simulation plan tables.
+    """
+    
+    def __init__(self):
+        
+        self.__version__ = "0.1.0"
+        self.table_format = "she.SimulationPlanTable"
+        
+        # Table metadata labels
+        self.version = "SS_VER"
+        self.format = "SS_FMT"
+        
+        # Store the less-used comments in a dict
+        self.comments = OrderedDict(((self.version, None),
+                                     (self.format, None),
+                                   ))
+        
+        # A list of columns in the desired order
+        self.all = self.comments.keys()
+
+class SimulationPlanTableFormat(object):
+    """
+        @brief A class defining the format for galaxy population priors tables. Only the simulation_plan_table_format
+               instance of this should generally be accessed, and it should not be changed.
+    """
+    
+    def __init__(self):
+        
+        # Get the metadata (contained within its own class)
+        self.meta = SimulationPlanTableMeta()
+        
+        # And a quick alias for it
+        self.m = self.meta
+        
+        # Get the version from the meta class
+        self.__version__ = self.m.__version__
+        
+        # Direct alias for a tuple of all metadata
+        self.meta_data = self.m.all
+        
+        # Dicts for less-used properties
+        self.is_optional = OrderedDict()
+        self.comments = OrderedDict()
+        self.dtypes = OrderedDict()
+        self.fits_dtypes = OrderedDict()
+        self.lengths = OrderedDict()
+        
+        def set_column_properties( name, is_optional=False, comment=None, dtype=">f4", fits_dtype="E",
+                                   length=1):
+            
+            assert name not in self.is_optional
+            
+            self.is_optional[name] = is_optional
+            self.comments[name] = comment
+            self.dtypes[name] = dtype
+            self.fits_dtypes[name] = fits_dtype
+            self.lengths[name] = length
+            
+            return name
+        
+        # Column names and info
+        
+        self.tag = set_column_properties("TAG", dtype="str", fits_dtype="10A",
+                                         comment="Tag to be added to file names for this batch, max length 10.")
+    
+        self.model_seed_min = set_column_properties("MSEED_MIN", dtype=">i8", fits_dtype="K",
+                                                    comment="Minimum model seed value for this batch.")
+        self.model_seed_max = set_column_properties("MSEED_MAX", dtype=">i8", fits_dtype="K",
+                                                    comment="Maximum model seed value for this batch.")
+        self.model_seed_step = set_column_properties("MSEED_STEP", dtype=">i8", fits_dtype="K",
+                                                     comment="Model seed step for this batch.")
+    
+        self.noise_seed_min = set_column_properties("MSEED_MIN", dtype=">i8", fits_dtype="K",
+                                                    comment="Minimum model seed value for this batch.")
+        self.noise_seed_max = set_column_properties("MSEED_MAX", dtype=">i8", fits_dtype="K",
+                                                    comment="Maximum model seed value for this batch.")
+        self.noise_seed_step = set_column_properties("MSEED_STEP", dtype=">i8", fits_dtype="K",
+                                                     comment="Model seed step for this batch.")
+        
+        self.suppress_noise = set_column_properties("SUP_NOISE", dtype="bool", fits_dtype="L")
+        self.num_detectors = set_column_properties("NUM_DETECTORS", dtype=">i2", fits_dtype="I")
+        self.num_galaxies = set_column_properties("NUM_GALAXIES", dtype=">i2", fits_dtype="I")
+        self.render_background = set_column_properties("RENDER_BKG", dtype="bool", fits_dtype="L")
+        
+        # A list of columns in the desired order
+        self.all = self.is_optional.keys()
+        
+        # A list of required columns in the desired order
+        self.all_required = []
+        for label in self.all:
+            if not self.is_optional[label]:
+                self.all_required.append(label)
+
+# Define an instance of this object that can be imported         
+simulation_plan_table_format = SimulationPlanTableFormat()
+
+# And a convient alias for it
+tf = simulation_plan_table_format
+
+
+def make_simulation_plan_table_header():
+    """
+        @brief Generate a header for a galaxy population table.
+        
+        @return header <OrderedDict>
+    """
+    
+    header = OrderedDict()
+    
+    header[tf.m.version] = tf.__version__
+    header[tf.m.format] = tf.m.table_format
+    
+    return header
+
+def initialise_simulation_plan_table(optional_columns = None):
+    """
+        @brief Initialise a galaxy population table.
+        
+        @return simulation_plan_table <astropy.Table>
+    """
+    
+    if optional_columns is None:
+        optional_columns = []
+    else:
+        # Check all optional columns are valid
+        for colname in optional_columns:
+            if colname not in tf.all:
+                raise ValueError("Invalid optional column name: " + colname)
+    
+    names = []
+    init_cols = []
+    dtypes = []
+    for colname in tf.all:
+        if (colname in tf.all_required) or (colname in optional_columns):
+            names.append(colname)
+            init_cols.append([])
+            dtypes.append((tf.dtypes[colname],tf.lengths[colname]))
+    
+    simulation_plan_table = Table(init_cols, names=names, dtype=dtypes)
+    
+    simulation_plan_table.meta = make_simulation_plan_table_header()
+    
+    return simulation_plan_table
