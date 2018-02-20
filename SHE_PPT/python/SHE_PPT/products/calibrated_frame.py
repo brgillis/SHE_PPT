bfe457e84f607e4b7afff14c0af603d344b9a984
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-02-20 18:29:58 +0000

    Fixing class location

--- SHE_PPT/python/SHE_PPT/products/calibrated_frame.py
+++ SHE_PPT/python/SHE_PPT/products/calibrated_frame.py
@@ -236,7 +236,7 @@ def create_vis_data_storage(filename, format="vis.reducedFrameVIS", version="0.0
     data_storage.format = format
     data_storage.version = version
     
-    data_storage.DataContainer = dss_dict.dataContainer()
+    data_storage.DataContainer = dataContainer()
     data_storage.DataContainer.FileName = filename
     data_storage.DataContainer.filestatus = filestatus
     
