b8f9e7fc22037048761c1f0a6bbc1d10a5115999
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-03-22 16:31:15 +0000

    Bad autofill! Bad!

--- SHE_PPT/python/SHE_PPT/she_frame_stack.py
+++ SHE_PPT/python/SHE_PPT/she_frame_stack.py
@@ -15,7 +15,6 @@
 # along with this library; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 #
-from builtins import None
 
 """
 File: python/SHE_PPT/she_frame_stack.py
