b44a1dd27494e8dabc76bb635ef85cc610679392
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-05-31 19:12:49 +0100

    Updating to use sample product

--- SHE_PPT/python/SHE_PPT/products/calibrated_frame.py
+++ SHE_PPT/python/SHE_PPT/products/calibrated_frame.py
@@ -20,14 +20,14 @@
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-from EuclidDmBindings.bas.imp.eso_stub import dataProduct
-from EuclidDmBindings.dpd.vis.calibratedframe_stub import DpdVisCalibratedFrame
-from EuclidDmBindings.ins_stub import baseInstrument
-from EuclidDmBindings.pro import le1_stub as le1
-from EuclidDmBindings.pro.le1 import vis_stub as le1vis
+from EuclidDmBindings.dpd.vis.raw.calibratedframe_stub import dpdVisCalibratedFrame
+import HeaderProvider.GenericHeaderProvider as HeaderProvider
 import EuclidDmBindings.pro.vis_stub as vis_pro
 from EuclidDmBindings.sys.dss_stub import dataContainer
-import HeaderProvider.GenericHeaderProvider as HeaderProvider
+
+from SHE_PPT.file_io import read_xml_product, find_aux_file
+
+sample_file_name = "SHE_PPT/sample_calibrated_frame.xml"
 
 
 def init():
@@ -35,7 +35,7 @@ def init():
         Adds some extra functionality to the DpdSheAstrometry product
     """
 
-    binding_class = DpdVisCalibratedFrame
+    binding_class = dpdVisCalibratedFrame
 
     # Add the data file name methods
 
@@ -106,117 +106,12 @@ def create_dpd_vis_calibrated_frame(filename = 'vis_calibrated_frame.fits'):
         @TODO fill in docstring
     """
 
-    dpd_vis_calibrated_frame = DpdVisCalibratedFrame
+    dpd_vis_calibrated_frame = read_xml_product(find_aux_file(sample_file_name), allow_pickled=False)
 
-    dpd_vis_calibrated_frame.Header = HeaderProvider.createGenericHeader("VIS")
-
-    dpd_vis_calibrated_frame.Data = create_vis_calibrated_frame(filename)
+    # Overwrite the header with a new one to update the creation date (among other things)
+    dpd_vis_calibrated_frame.Header = HeaderProvider.createGenericHeader("SHE")
 
     return dpd_vis_calibrated_frame
-
-# Add a useful alias
-create_calibrated_frame_product = create_dpd_vis_calibrated_frame
-
-def create_vis_calibrated_frame(filename = None):
-    """
-        @TODO fill in docstring
-    """
-
-    vis_calibrated_frame = vis_pro.visCalibratedFrame()
-
-    # Attributes inherited from imgBaseFrame
-
-    vis_calibrated_frame.ImgType = create_img_type()
-    vis_calibrated_frame.ImgNumber = 36
-    vis_calibrated_frame.AxisNumber = 2
-    vis_calibrated_frame.AxisLengths = "4096 4132"
-    vis_calibrated_frame.DataSize = -32
-    vis_calibrated_frame.DataLength = 4096 * 4132
-
-    # Attributes inherited from baseFrameVis
-
-    vis_calibrated_frame.Instrument = create_vis_instrument()
-    vis_calibrated_frame.Filter = "VIS"
-    vis_calibrated_frame.InstrumentMode = "Science"
-    vis_calibrated_frame.ObservationMode = "ScienceWide"
-    vis_calibrated_frame.ReconsOrbit = create_vis_recons_orbit()
-    vis_calibrated_frame.Readout = create_vis_readout()
-    vis_calibrated_frame.ShutterUnit = create_vis_shutter_unit()
-    vis_calibrated_frame.CalibUnit = create_vis_calib_unit()
-    vis_calibrated_frame.ChargedInduced = create_vis_charged_induced()
-
-    # Attributes unique to calibratedFrameVis
-
-    vis_calibrated_frame.DataStorage = create_vis_data_storage(filename, "vis.reducedFrameVIS")
-
-    return vis_calibrated_frame
-
-def create_img_type():
-
-    img_type = dataProduct()
-
-    img_type.Category = "SCIENCE"
-    img_type.FirstType = "OBJECT"
-    img_type.SecondType = "SKY"
-    img_type.ThirdType = "WIDE"
-    img_type.Technique = "IMAGE"
-
-    return img_type
-
-def create_vis_instrument():
-
-    instrument = baseInstrument()
-
-    instrument.InstrumentName = "VIS Instrument"
-    instrument.TelescopeName = "Telescope"
-
-    return instrument
-
-def create_vis_recons_orbit():
-
-    recons_orbit = le1.spacecraftOrbit()
-
-    recons_orbit.Position = "999.0 999.0 999.0"
-    recons_orbit.Velocity = "999.0 999.0 999.0"
-    recons_orbit.SolarAspectAngle = 999.0
-
-    return recons_orbit
-
-def create_vis_readout():
-
-    readout = le1vis.visReadoutMode()
-
-    readout.ReadoutModeMethod = "NominalScience"
-    readout.StartTime = "2006-05-05T18:00:00.000Z"
-    readout.ParallelRegFrequency = 999.0
-    readout.SerialRegFrequency = 999.0
-
-    return readout
-
-def create_vis_shutter_unit():
-
-    shutter_unit = le1vis.shutterUnit()
-
-    shutter_unit.Status = "OPENED"
-
-    return shutter_unit
-
-def create_vis_calib_unit():
-
-    calib_unit = le1.calibUnit()
-
-    calib_unit.Status = False
-
-    return calib_unit
-
-def create_vis_charged_induced():
-
-    charged_induced = le1vis.chargedInduced()
-
-    charged_induced.Status = False
-    charged_induced.IntensityLevel = 999.0
-
-    return charged_induced
 
 def create_vis_data_storage(filename, format = "vis.reducedFrameVIS", version = "0.0", filestatus = "PROPOSED"):
 
