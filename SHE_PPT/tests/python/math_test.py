7c1f9d430f00c5a0e28d0c157c5f0f3b47e5bc6f
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-06-21 16:55:36 +0100

    Can't really test this this way

--- SHE_PPT/tests/python/math_test.py
+++ SHE_PPT/tests/python/math_test.py
@@ -55,8 +55,7 @@ class Test_math():
         # Test it matches results for regular regression if we don't use
         # weights
         unweighted_results = linregress_with_errors(x, y)
-        slope, intercept, _, _, intercept_err = linregress(x, y)
+        slope, intercept, _, _, _ = linregress(x, y)
 
         assert_almost_equal(unweighted_results.slope, slope)
         assert_almost_equal(unweighted_results.intercept, intercept)
-        assert_almost_equal(unweighted_results.intercept_err, intercept_err)
