38376bcc6f3f01252a74ec3406a0c08e6a414925
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-08-06 16:31:00 +0200

    Testing without allowing pickled fallback now

--- SHE_PPT/tests/python/calibrated_frame_product_test.py
+++ SHE_PPT/tests/python/calibrated_frame_product_test.py
@@ -63,10 +63,10 @@ class TestCalibratedFrameProduct(object):
 
         # Save the product in an XML file
         filename = tmpdir.join("vis_calibrated_frame.xml")
-        write_xml_product(product, filename)
+        write_xml_product(product, filename, allow_pickled=False)
 
         # Read back the XML file
-        loaded_product = read_xml_product(filename)
+        loaded_product = read_xml_product(filename, allow_pickled=False)
 
         # Check that the filenames match
         assert loaded_product.get_data_filename() == sub_data_filename
--- SHE_PPT/tests/python/shear_estimates_product_test.py
+++ SHE_PPT/tests/python/shear_estimates_product_test.py
@@ -77,10 +77,10 @@ class TestShearEstimatesProduct(object):
 
         # Save the product in an XML file
         filename = tmpdir.join("she_shear_estimates.xml")
-        write_xml_product(product, filename)
+        write_xml_product(product, filename, allow_pickled=False)
 
         # Read back the XML file
-        loaded_product = read_xml_product(filename)
+        loaded_product = read_xml_product(filename, allow_pickled=False)
 
         # Check that the filenames coincide
         assert loaded_product.get_BFD_filename() == b_filename
