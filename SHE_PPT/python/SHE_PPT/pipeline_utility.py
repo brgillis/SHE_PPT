3ad3e9ea645b10e3abc22104db98fbce1d21bcb4
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-08-09 16:59:08 +0100

    Adding pipeline_utility.py to index

---
+++ SHE_PPT/python/SHE_PPT/pipeline_utility.py
@@ -0,0 +1,88 @@
+""" @file pipeline_utility.py
+
+    Created 9 Aug 2018
+
+    Misc. utility functions for the pipeline.
+"""
+
+__updated__ = "2018-08-09"
+
+# Copyright (C) 2012-2020 Euclid Science Ground Segment
+#
+# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+import os
+from shutil import copyfile
+
+from SHE_PPT import magic_values as mv
+from SHE_PPT.file_io import read_xml_product
+from SHE_PPT.logging import getLogger
+
+
+def archive_product(product_filename, archive_dir, workdir):
+    """ Copies an already-written data product to an archive directory.
+
+        Parameters
+        ----------
+        product_filename : string
+            The (unqualified) name of the product to copy
+        archive_dir : string
+            The root of the archive directory (note, the most-specific part of the workdir path (normally "workspace")
+            will be added after this to keep separate runs from conflicting).
+        workdir : string
+            The working directory for this task
+
+    """
+
+    logger = getLogger(mv.logger_name)
+
+    # Start by figuring out the subdirectory to store it in, based off of the workdir we're using
+    subdir = os.path.split(workdir)[1]
+    full_archive_dir = os.path.join(archive_dir, subdir)
+
+    # The filename will likely also contain a subdir, so figure that out
+    product_subpath = os.path.split(product_filename)[0]
+
+    # Make the directory to store it in
+    full_archive_subdir = os.path.join(full_archive_dir, product_subpath)
+    full_archive_datadir = os.path.join(full_archive_dir, "data")
+    if not os.path.exists(full_archive_subdir):
+        os.makedirs(full_archive_subdir)
+    if not os.path.exists(full_archive_datadir):
+        os.makedirs(full_archive_datadir)
+
+    # Copy the file to the archive
+    qualified_filename = os.path.join(workdir, product_filename)
+    qualified_archive_product_filename = os.path.join(full_archive_dir, product_filename)
+    copyfile(qualified_filename, qualified_archive_product_filename)
+
+    # Copy any files it points to to the archive as well
+    try:
+        p = read_xml_product(qualified_filename)
+
+        # Remove all files this points to
+        if hasattr(p, "get_all_filenames"):
+            data_filenames = p.get_all_filenames()
+            for data_filename in data_filenames:
+                if data_filename is not None and data_filename != "default_filename.fits" and data_filename != "":
+                    qualified_data_filename = os.path.join(workdir, data_filename)
+                    qualified_archive_data_filename = os.path.join(full_archive_dir, data_filename)
+                    copyfile(qualified_data_filename, qualified_archive_data_filename)
+
+        else:
+            logger.warn("Product " + qualified_filename + " has no 'get_all_filenames' method.")
+
+    except Exception as e:
+        logger.warn("Failsafe exception block triggered when trying to save statistics product in archive. " +
+                    "Exception was: " + str(e))
+
+    return
