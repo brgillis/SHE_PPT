06c8078ab716c4db0957af15d23ccd518e229f62
user <user@localhost>
2019-06-27 12:43:53 +0200

    find_file now deals with HOME/

--- SHE_PPT/python/SHE_PPT/file_io.py
+++ SHE_PPT/python/SHE_PPT/file_io.py
@@ -318,6 +318,9 @@ def find_file(filename, path=None):
         return find_aux_file(filename[4:])
     elif filename[0:5] == "CONF/":
         return find_conf_file(filename[5:])
+    elif filename[0:5] == "HOME/":
+        path = os.path.join(os.getenv('HOME'),os.path.dirname(filename[5:]))
+        return find_file_in_path(os.path.basename(filename), path)
     elif filename[0] == "/":
         if not os.path.exists(filename):
             raise RuntimeError("File " + filename + " cannot be found.")
