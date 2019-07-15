90ae9d7434b6782d2f3139234d85d34f1ae0eb39
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2019-07-15 14:10:10 +0100

    Cleaning up tests

--- SHE_PPT/tests/python/shear_bias_statistics_product_test.py
+++ SHE_PPT/tests/python/shear_bias_statistics_product_test.py
@@ -1,8 +1,8 @@
-""" @file shear_estimates_product_test.py
+""" @file shear_bias_statistics_product_test.py
 
-    Created 9 Oct 2017
+    Created 15 July 2019
 
-    Unit tests for the shear estimates data product.
+    Unit tests for the shear bias statistics data product.
 """
 
 # Copyright (C) 2012-2020 Euclid Science Ground Segment
@@ -19,12 +19,13 @@
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA
 
-__updated__ = "2019-06-25"
+__updated__ = "2019-07-15"
 
+import SHE_PPT
 from SHE_PPT.file_io import (read_xml_product, write_xml_product,
                              read_pickled_product, write_pickled_product)
 from SHE_PPT.math import LinregressStatistics, BFDSumStatistics
-from SHE_PPT.products import shear_bias_stats as prod
+from SHE_PPT.products import shear_bias_statistics as prod
 import numpy as np
 
 
@@ -63,42 +64,32 @@ class TestShearBiasStatsProduct(object):
                 stats[method] = (LinregressStatistics(x1, y1, y_err),
                                  LinregressStatistics(x2, y2, y_err),)
             else:
-                stats[method] = (BFDSumStatistics(sums_for_bfd), None)
+                stats[method] = BFDSumStatistics(sums_for_bfd)
         # Create the product
-        product = prod.create_dpd_shear_bias_statistics(BFD_bfd_statistics=stats["BFD"][0],
-                                                        KSB_g1_statistics=stats[
-                                                            "KSB"][0],
-                                                        KSB_g2_statistics=stats[
-                                                            "KSB"][1],
-                                                        LensMC_g1_statistics=stats[
-                                                            "LensMC"][0],
-                                                        LensMC_g2_statistics=stats[
-                                                            "LensMC"][1],
-                                                        MomentsML_g1_statistics=stats[
-                                                            "MomentsML"][0],
-                                                        MomentsML_g2_statistics=stats[
-                                                            "MomentsML"][1],
-                                                        REGAUSS_g1_statistics=stats[
-                                                            "REGAUSS"][0],
+        product = prod.create_dpd_shear_bias_statistics(BFD_statistics=stats["BFD"],
+                                                        KSB_g1_statistics=stats["KSB"][0],
+                                                        KSB_g2_statistics=stats["KSB"][1],
+                                                        LensMC_g1_statistics=stats["LensMC"][0],
+                                                        LensMC_g2_statistics=stats["LensMC"][1],
+                                                        MomentsML_g1_statistics=stats["MomentsML"][0],
+                                                        MomentsML_g2_statistics=stats["MomentsML"][1],
+                                                        REGAUSS_g1_statistics=stats["REGAUSS"][0],
                                                         REGAUSS_g2_statistics=stats["REGAUSS"][1])
 
         # Check that it validates the schema
         product.validateBinding()
 
         # Check that it was inited with the proper values
-        assert product.get_BFD_statistics() == stats["BFD"][0]
+        assert product.get_BFD_statistics() == stats["BFD"]
         assert product.get_KSB_statistics() == stats["KSB"]
         assert product.get_LensMC_statistics() == stats["LensMC"]
         assert product.get_MomentsML_statistics() == stats["MomentsML"]
         assert product.get_REGAUSS_statistics() == stats["REGAUSS"]
 
-        # Check the general get method works#
+        # Check the general get method works
         stats2 = {}
         for method in ("BFD", "KSB", "LensMC", "MomentsML", "REGAUSS"):
-            if not method == 'BFD':
-                assert product.get_method_statistics(method) == stats[method]
-            else:
-                assert product.get_method_statistics(method) == stats[method][0]
+            assert product.get_method_statistics(method) == stats[method]
             x1 = np.linspace(0, n - 1, n, endpoint=True)
             x2 = np.linspace(0, n - 1, n, endpoint=True)
             y_err = 0.25 * np.ones_like(x1)
@@ -108,15 +99,13 @@ class TestShearBiasStatsProduct(object):
                 stats2[method] = (LinregressStatistics(x1, y1, y_err),
                                   LinregressStatistics(x2, y2, y_err),)
             else:
-                stats2[method] = (BFDSumStatistics(sums_for_bfd), None)
+                stats2[method] = BFDSumStatistics(sums_for_bfd)
 
             product.set_method_statistics(method, *stats2[method])
 
-            if not method == 'BFD':
-                assert product.get_method_statistics(method) == stats2[method]
-            else:
-                assert product.get_method_statistics(method) == stats2[method][0]
-        pass
+            assert product.get_method_statistics(method) == stats2[method]
+
+        return
 
     def test_xml_writing_and_reading(self, tmpdir):
 
@@ -149,23 +138,16 @@ class TestShearBiasStatsProduct(object):
                 stats[method] = (LinregressStatistics(x1, y1, y_err),
                                  LinregressStatistics(x2, y2, y_err),)
             else:
-                stats[method] = (BFDSumStatistics(sums_for_bfd), None)
+                stats[method] = BFDSumStatistics(sums_for_bfd)
         # Create the product
-        product = prod.create_dpd_shear_bias_statistics(BFD_bfd_statistics=stats["BFD"][0],
-                                                        KSB_g1_statistics=stats[
-                                                            "KSB"][0],
-                                                        KSB_g2_statistics=stats[
-                                                            "KSB"][1],
-                                                        LensMC_g1_statistics=stats[
-                                                            "LensMC"][0],
-                                                        LensMC_g2_statistics=stats[
-                                                            "LensMC"][1],
-                                                        MomentsML_g1_statistics=stats[
-                                                            "MomentsML"][0],
-                                                        MomentsML_g2_statistics=stats[
-                                                            "MomentsML"][1],
-                                                        REGAUSS_g1_statistics=stats[
-                                                            "REGAUSS"][0],
+        product = prod.create_dpd_shear_bias_statistics(BFD_statistics=stats["BFD"],
+                                                        KSB_g1_statistics=stats["KSB"][0],
+                                                        KSB_g2_statistics=stats["KSB"][1],
+                                                        LensMC_g1_statistics=stats["LensMC"][0],
+                                                        LensMC_g2_statistics=stats["LensMC"][1],
+                                                        MomentsML_g1_statistics=stats["MomentsML"][0],
+                                                        MomentsML_g2_statistics=stats["MomentsML"][1],
+                                                        REGAUSS_g1_statistics=stats["REGAUSS"][0],
                                                         REGAUSS_g2_statistics=stats["REGAUSS"][1])
 
         # Save the product in an XML file
@@ -179,17 +161,11 @@ class TestShearBiasStatsProduct(object):
         assert loaded_product.get_BFD_statistics().A11 == stats["BFD"][0].A11
         assert loaded_product.get_KSB_statistics()[0].ym == stats["KSB"][0].ym
         assert loaded_product.get_KSB_statistics()[1].ym == stats["KSB"][1].ym
-        assert loaded_product.get_LensMC_statistics()[0].ym == stats[
-            "LensMC"][0].ym
-        assert loaded_product.get_LensMC_statistics()[1].ym == stats[
-            "LensMC"][1].ym
-        assert loaded_product.get_MomentsML_statistics()[0].ym == stats[
-            "MomentsML"][0].ym
-        assert loaded_product.get_MomentsML_statistics()[1].ym == stats[
-            "MomentsML"][1].ym
-        assert loaded_product.get_REGAUSS_statistics()[0].ym == stats[
-            "REGAUSS"][0].ym
-        assert loaded_product.get_REGAUSS_statistics()[1].ym == stats[
-            "REGAUSS"][1].ym
+        assert loaded_product.get_LensMC_statistics()[0].ym == stats["LensMC"][0].ym
+        assert loaded_product.get_LensMC_statistics()[1].ym == stats["LensMC"][1].ym
+        assert loaded_product.get_MomentsML_statistics()[0].ym == stats["MomentsML"][0].ym
+        assert loaded_product.get_MomentsML_statistics()[1].ym == stats["MomentsML"][1].ym
+        assert loaded_product.get_REGAUSS_statistics()[0].ym == stats["REGAUSS"][0].ym
+        assert loaded_product.get_REGAUSS_statistics()[1].ym == stats["REGAUSS"][1].ym
 
-        pass
+        return
