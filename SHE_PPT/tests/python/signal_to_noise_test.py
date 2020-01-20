cedd9c0afba16bb406ca30a47a00e70754153d9d
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2020-01-20 16:10:24 +0000

    Fixing another variable name

--- SHE_PPT/tests/python/signal_to_noise_test.py
+++ SHE_PPT/tests/python/signal_to_noise_test.py
@@ -71,7 +71,7 @@ class TestCase:
 
         self.sync_datastack = DataSync("testdata/sync.conf", "testdata/test_data_stack.txt")
         self.sync_datastack.download()
-        self.qualified_data_images_filename = self.sync.absolutePath("SHE_CTE/data/data_images.json")
+        self.qualified_data_images_filename = self.sync_datastack.absolutePath("SHE_CTE/data/data_images.json")
         
         self.qualified_data_images_filename = localTestFile(test_data_location, "SHE_CTE/data/data_images.json")
         assert os.path.isfile(self.qualified_data_images_filename), f"Cannot find file: {self.qualified_data_images_filename}"
