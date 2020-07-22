43d84d003a1e7f87370fe9fe43e90fd3f8548479
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2020-07-22 17:15:23 +0200

    Adding product for reconciled measurements + test

---
+++ SHE_PPT/tests/python/she_reconciled_measurements_product_test.py
@@ -0,0 +1,66 @@
+""" @file she_reconciled_measurements_product_test.py
+
+    Created 17 Nov 2017
+
+    Unit tests for the reconciled shear measurements data product.
+"""
+
+# Copyright (C) 2012-2020 Euclid Science Ground Segment
+#
+# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+__updated__ = "2020-07-22"
+
+from SHE_PPT.file_io import (read_xml_product, write_xml_product,
+                             read_pickled_product, write_pickled_product)
+from SHE_PPT.products import she_reconciled_measurements as prod
+
+
+class TestReconciledShearEstimatesProduct(object):
+    """A collection of tests for the shear estimates data product.
+
+    """
+
+    def test_validation(self):
+
+        # Create the product
+        subfilename = "foo.fits"
+        product = prod.create_dpd_she_reconciled_measurements(LensMC_filename=subfilename)
+
+        # Check that it validates the schema
+        product.validateBinding()
+
+        # Check that it was inited with the proper filename
+        assert product.get_LensMC_filename() == "data/" + subfilename
+
+        return
+
+    def test_xml_writing_and_reading(self, tmpdir):
+
+        # Create the product
+        product = prod.create_dpd_she_reconciled_measurements()
+
+        # Change the fits filenames
+        subfilename = "test_file.fits"
+        product.set_LensMC_filename(subfilename)
+
+        # Save the product in an XML file
+        filename = "she_she_reconciled_measurements.xml"
+        write_xml_product(product, filename, workdir=str(tmpdir), allow_pickled=False)
+
+        # Read back the XML file
+        loaded_product = read_xml_product(filename, workdir=str(tmpdir), allow_pickled=False)
+
+        # Check that the filenames match
+        assert loaded_product.get_LensMC_filename() == "data/" + subfilename
+
+        return
