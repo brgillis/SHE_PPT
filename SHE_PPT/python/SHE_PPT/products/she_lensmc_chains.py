dc977d7bbc733db5f7c075331b93a0fc22225cbc
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2020-09-01 15:57:15 +0200

    Adding first draft of LensMC Chains data product

---
+++ SHE_PPT/python/SHE_PPT/products/she_lensmc_chains.py
@@ -0,0 +1,104 @@
+""" @file she_lensmc_chains.py
+
+    Created 1 Sep 2020
+
+    Functions to create and output a lensmc_chains_data data product.
+
+    Origin: OU-SHE - Needs to be implemented in data model. Output from Calibration pipeline
+    and input to Analysis pipeline; must be persistent in archive.
+"""
+
+# Copyright (C) 2012-2020 Euclid Science Ground Segment
+#
+# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+
+__updated__ = "2020-09-01"
+
+import pickle
+
+from SHE_PPT.file_io import read_xml_product, find_aux_file, get_data_filename_from_product, set_data_filename_of_product
+import ST_DM_HeaderProvider.GenericHeaderProvider as HeaderProvider
+from ST_DataModelBindings.dpd.she.lensmctraining_stub import dpdSheLensMcChains
+
+sample_file_name = "SHE_PPT/sample_lensmc_chains.xml"
+
+
+def init():
+    """
+        Initialisers for LensMC training
+    """
+
+    binding_class = dpdSheLensMcChains
+
+    # Add the data file name methods
+
+    binding_class.set_filename = __set_filename
+    binding_class.get_filename = __get_filename
+    binding_class.set_data_filename = __set_filename
+    binding_class.get_data_filename = __get_filename
+
+    binding_class.get_all_filenames = __get_all_filenames
+
+    binding_class.has_files = False
+
+    return
+
+
+def __set_filename(self, filename):
+    set_data_filename_of_product(self, filename, "DataStorage")
+
+
+def __get_filename(self):
+    return get_data_filename_from_product(self, "DataStorage")
+
+
+def __get_all_filenames(self):
+
+    all_filenames = [self.get_filename()]
+
+    return all_filenames
+
+
+def create_dpd_she_lensmc_chains(filename=None):
+    """
+        @TODO fill in docstring
+    """
+
+    dpd_she_lensmc_chains = read_xml_product(find_aux_file(sample_file_name))
+
+    dpd_she_lensmc_chains.Header = HeaderProvider.create_generic_header("SHE")
+
+    if filename:
+        __set_filename(dpd_she_lensmc_chains, filename)
+    return dpd_she_lensmc_chains
+
+
+# Add a useful alias
+create_lensmc_chains_data_product = create_dpd_she_lensmc_chains
+
+
+def create_she_lensmc_chains(filename=None):
+    """
+        @TODO fill in docstring
+    """
+
+    she_lensmc_chains = SheLensMcChains()
+
+    she_lensmc_chains.format = "she.lensMcChains"
+    she_lensmc_chains.version = "8.0"
+
+    she_lensmc_chains.DataContainer = DataContainer()
+    she_lensmc_chains.DataContainer.FileName = filename
+    she_lensmc_chains.DataContainer.filestatus = "PROPOSED"
+
+    return she_lensmc_chains
