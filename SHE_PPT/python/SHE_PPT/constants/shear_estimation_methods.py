3062b4d67e610c7bbc7f804382a0b0f5a7252ee4
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2021-01-06 18:11:44 +0100

    Adding constants for shear estimation methods

---
+++ SHE_PPT/python/SHE_PPT/constants/shear_estimation_methods.py
@@ -0,0 +1,37 @@
+""" @file __init__.py
+
+    Created 6 Jan 2021
+"""
+
+# Copyright (C) 2012-2020 Euclid Science Ground Segment
+#
+# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+
+from ..table_formats.she_ksb_measurements import tf as ksbm_tf
+from ..table_formats.she_lensmc_measurements import tf as lmcm_tf
+from ..table_formats.she_momentsml_measurements import tf as mmlm_tf
+from ..table_formats.she_regauss_measurements import tf as regm_tf
+
+KEY_KSB = "KSB"
+KEY_REGAUSS = "REGAUSS"
+KEY_MOMENTSML = "MomentsML"
+KEY_LENSMC = "LensMC"
+
+D_SHEAR_ESTIMATION_METHOD_TABLE_FORMATS = {KEY_KSB: ksbm_tf,
+                                           KEY_REGAUSS: regm_tf,
+                                           KEY_MOMENTSML: mmlm_tf,
+                                           KEY_LENSMC: lmcm_tf}
+
+METHODS = D_SHEAR_ESTIMATION_METHOD_TABLE_FORMATS.keys()
+
+NUM_METHODS = len(METHODS)
