883733a106d1aa666673f2d30addcb7e83d6ef49
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2022-01-19 17:16:48 +0000

    Updating test data location to new folder for DM 8.1 test data

--- SHE_PPT/python/SHE_PPT/constants/test_data.py
+++ SHE_PPT/python/SHE_PPT/constants/test_data.py
@@ -29,7 +29,7 @@ SYNC_CONF = join(SYNC_LOCATION, "sync.conf")
 TEST_FILES_MDB = join(SYNC_LOCATION, "test_mdb.txt")
 TEST_FILES_DATA_STACK = join(SYNC_LOCATION, "test_data_stack.txt")
 
-TEST_DATA_LOCATION = "SHE_PPT_8_7"
+TEST_DATA_LOCATION = "SHE_PPT_8_11"
 TEST_DATADIR = "/tmp"
 
 # Files from the MDB list
