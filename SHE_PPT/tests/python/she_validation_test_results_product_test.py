1f9cbc281c9f57fcf3bee5d9440482f3972cdb64
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2022-01-19 18:00:02 +0000

    Fixing Observation mode to be an allowed value

--- SHE_PPT/tests/python/she_validation_test_results_product_test.py
+++ SHE_PPT/tests/python/she_validation_test_results_product_test.py
@@ -25,10 +25,10 @@ import os
 import pytest
 
 from ElementsServices.DataSync import DataSync
-from SHE_PPT.constants.test_data import (SYNC_CONF, TEST_FILES_DATA_STACK, TEST_DATA_LOCATION,
-                                         VIS_CALIBRATED_FRAME_LISTFILE_FILENAME, MER_FINAL_CATALOG_LISTFILE_FILENAME,
-                                         VIS_STACKED_FRAME_PRODUCT_FILENAME,)
-from SHE_PPT.file_io import read_xml_product, write_xml_product, read_listfile
+from SHE_PPT.constants.test_data import (MER_FINAL_CATALOG_LISTFILE_FILENAME, SYNC_CONF, TEST_DATA_LOCATION,
+                                         TEST_FILES_DATA_STACK, VIS_CALIBRATED_FRAME_LISTFILE_FILENAME,
+                                         VIS_STACKED_FRAME_PRODUCT_FILENAME, )
+from SHE_PPT.file_io import read_listfile, read_xml_product, write_xml_product
 from SHE_PPT.products import she_validation_test_results as prod
 
 
@@ -37,9 +37,8 @@ class TestValidationTestResults(object):
 
     """
 
-    @pytest.fixture(autouse=True)
+    @pytest.fixture(autouse = True)
     def setup(self):
-
         # Download the data stack files from WebDAV
 
         self.sync_datastack = DataSync(SYNC_CONF, TEST_FILES_DATA_STACK)
@@ -56,7 +55,6 @@ class TestValidationTestResults(object):
 
     @classmethod
     def setup_class(cls):
-
         cls.filename = "she_validation_test_results.xml"
         cls.filename_zero = "she_validation_test_results_0.xml"
         cls.filename_three = "she_validation_test_results_3.xml"
@@ -65,7 +63,7 @@ class TestValidationTestResults(object):
         cls.filename_obs = "she_validation_test_results_obs.xml"
 
         cls.source_pipeline = "sheReconciliation"
-        cls.observation_mode = "ScienceDeep"
+        cls.observation_mode = "WIDE"
         cls.num_exposures = 3
 
     @classmethod
@@ -73,17 +71,16 @@ class TestValidationTestResults(object):
         return
 
     def test_xml_writing_and_reading(self):
-
         # Create the product
-        product = prod.create_dpd_she_validation_test_results(source_pipeline=self.source_pipeline,
-                                                              observation_mode=self.observation_mode,
-                                                              num_exposures=self.num_exposures)
+        product = prod.create_dpd_she_validation_test_results(source_pipeline = self.source_pipeline,
+                                                              observation_mode = self.observation_mode,
+                                                              num_exposures = self.num_exposures)
 
         # Save the product in an xml file
-        write_xml_product(product, self.filename, workdir=self.workdir)
+        write_xml_product(product, self.filename, workdir = self.workdir)
 
         # Read back the xml file
-        loaded_product = read_xml_product(self.filename, workdir=self.workdir)
+        loaded_product = read_xml_product(self.filename, workdir = self.workdir)
 
         # Check that it's the same
         assert loaded_product.Data.SourcePipeline == self.source_pipeline
@@ -93,80 +90,77 @@ class TestValidationTestResults(object):
         # Try with zero and multiple tests
 
         # Create the product
-        product_zero = prod.create_dpd_she_validation_test_results(num_tests=0)
+        product_zero = prod.create_dpd_she_validation_test_results(num_tests = 0)
 
         # Save the product in an xml file
-        write_xml_product(product_zero, self.filename_zero, workdir=self.workdir)
+        write_xml_product(product_zero, self.filename_zero, workdir = self.workdir)
 
         # Read back the xml file
-        loaded_product_zero = read_xml_product(self.filename_zero, workdir=self.workdir)
+        loaded_product_zero = read_xml_product(self.filename_zero, workdir = self.workdir)
 
         assert len(loaded_product_zero.Data.ValidationTestList) == 0
 
         # Create the product
-        product_three = prod.create_dpd_she_validation_test_results(num_tests=3)
+        product_three = prod.create_dpd_she_validation_test_results(num_tests = 3)
 
         # Save the product in an xml file
-        write_xml_product(product_three, self.filename_zero, workdir=self.workdir)
+        write_xml_product(product_three, self.filename_zero, workdir = self.workdir)
 
         # Read back the xml file
-        loaded_product_three = read_xml_product(self.filename_zero, workdir=self.workdir)
+        loaded_product_three = read_xml_product(self.filename_zero, workdir = self.workdir)
 
         assert len(loaded_product_three.Data.ValidationTestList) == 3
 
     def test_mer_final_catalog_reference(self):
-
         # Read the MER catalogs listfile
         mer_final_catalog_list = read_listfile(os.path.join(self.workdir, MER_FINAL_CATALOG_LISTFILE_FILENAME))
 
         # Read the MER Final Catalog product
-        mer_final_catalog_product = read_xml_product(mer_final_catalog_list[0], workdir=self.workdir)
+        mer_final_catalog_product = read_xml_product(mer_final_catalog_list[0], workdir = self.workdir)
 
         # Create the product
-        product = prod.create_dpd_she_validation_test_results(reference_product=mer_final_catalog_product)
+        product = prod.create_dpd_she_validation_test_results(reference_product = mer_final_catalog_product)
 
         # Save the product in an xml file
-        write_xml_product(product, self.filename_tile, workdir=self.workdir)
+        write_xml_product(product, self.filename_tile, workdir = self.workdir)
 
         # Read back the xml file
-        loaded_product = read_xml_product(self.filename_tile, workdir=self.workdir)
+        loaded_product = read_xml_product(self.filename_tile, workdir = self.workdir)
 
         assert loaded_product.Data.TileId == mer_final_catalog_product.Data.TileIndex
 
     def test_vis_calibrated_frame_reference(self):
-
         # Read the MER catalogs listfile
         vis_calibrated_frame_list = read_listfile(os.path.join(self.workdir, VIS_CALIBRATED_FRAME_LISTFILE_FILENAME))
 
         # Read the MER Final Catalog product
-        vis_calibrated_frame_product = read_xml_product(vis_calibrated_frame_list[0], workdir=self.workdir)
+        vis_calibrated_frame_product = read_xml_product(vis_calibrated_frame_list[0], workdir = self.workdir)
 
         # Create the product
-        product = prod.create_dpd_she_validation_test_results(reference_product=vis_calibrated_frame_product)
+        product = prod.create_dpd_she_validation_test_results(reference_product = vis_calibrated_frame_product)
 
         # Save the product in an xml file
-        write_xml_product(product, self.filename_exp, workdir=self.workdir)
+        write_xml_product(product, self.filename_exp, workdir = self.workdir)
 
         # Read back the xml file
-        loaded_product = read_xml_product(self.filename_exp, workdir=self.workdir)
+        loaded_product = read_xml_product(self.filename_exp, workdir = self.workdir)
 
         assert loaded_product.Data.ExposureProductId == vis_calibrated_frame_product.Header.ProductId
         assert loaded_product.Data.ObservationId == vis_calibrated_frame_product.Data.ObservationSequence.ObservationId
 
     def test_vis_stacked_frame_reference(self):
-
         # Read the MER Final Catalog product
-        vis_stacked_frame_product = read_xml_product(VIS_STACKED_FRAME_PRODUCT_FILENAME, workdir=self.workdir)
+        vis_stacked_frame_product = read_xml_product(VIS_STACKED_FRAME_PRODUCT_FILENAME, workdir = self.workdir)
 
         # Create the product
-        product = prod.create_dpd_she_validation_test_results(reference_product=vis_stacked_frame_product,
-                                                              num_exposures=self.num_exposures)
+        product = prod.create_dpd_she_validation_test_results(reference_product = vis_stacked_frame_product,
+                                                              num_exposures = self.num_exposures)
 
         # Save the product in an xml file
-        write_xml_product(product, self.filename_obs, workdir=self.workdir)
+        write_xml_product(product, self.filename_obs, workdir = self.workdir)
 
         # Read back the xml file
-        loaded_product = read_xml_product(self.filename_obs, workdir=self.workdir)
+        loaded_product = read_xml_product(self.filename_obs, workdir = self.workdir)
 
         assert loaded_product.Data.ObservationId == vis_stacked_frame_product.Data.ObservationId
         assert loaded_product.Data.NumberExposures == self.num_exposures
