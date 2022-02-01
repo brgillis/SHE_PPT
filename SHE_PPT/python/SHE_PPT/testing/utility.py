7f0e44f47b831fe049a461ca2f5746da009d1c82
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2022-02-01 16:53:18 +0000

    Cleanup

--- SHE_PPT/python/SHE_PPT/testing/utility.py
+++ SHE_PPT/python/SHE_PPT/testing/utility.py
@@ -20,6 +20,7 @@ __updated__ = "2021-08-16"
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA
+
 import os
 from argparse import Namespace
 from typing import Any, Dict, Optional
@@ -125,7 +126,6 @@ class SheTestCase:
     @pytest.fixture(scope = 'class')
     def class_setup(self, tmpdir_factory):
         self._finalize_class_setup(tmpdir_factory)
-        
         return self
 
     def _finalize_class_setup(self, tmpdir_factory):
