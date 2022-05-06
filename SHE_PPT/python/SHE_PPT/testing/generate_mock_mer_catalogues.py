387fd63f899a6ed00f8ad75101d5537161404b99
Gordon Gibb <gordon.gibb@ed.ac.uk>
2022-05-06 12:24:47 +0100

    Optimised creation of MER final catalogue table, return object_id

--- SHE_PPT/python/SHE_PPT/testing/generate_mock_mer_catalogues.py
+++ SHE_PPT/python/SHE_PPT/testing/generate_mock_mer_catalogues.py
@@ -5,7 +5,7 @@
     Utilities to generate mock mer final catalogues for smoke tests.
 """
 
-__updated__ = "2022-03-24"
+__updated__ = "2022-05-04"
 
 # Copyright (C) 2012-2022 Euclid Science Ground Segment
 #
@@ -43,29 +43,37 @@ def create_catalogue(obj_coords=[], workdir="."):
 
        Outputs:
          - product_filename: The filename of the created dpdMerFinalCatalog product
+         - object_ids: a list of the MER object ids of each object
     """
 
     logger.info("Creating MER final catalogue table with %d object(s)"%len(obj_coords))
     
     #initialise the mer_final_catalog table
     table = initialise_mer_final_catalog()
+
+    object_ids = []
+
+    n_objs = len(obj_coords)
     
-    #loop over all objects and add them to the table
-    for i, coords in enumerate(obj_coords):
-        obj_id = i+1
-        
-        #TODO: Improve how this done as adding rows to a table is ASTRONOMICALLY slow.
-        # maybe initialise the table with these columns instead?
-        table.add_row(vals={
-                            tf.ID: obj_id,
-                            tf.gal_x_world: coords.ra.deg,
-                            tf.gal_y_world: coords.dec.deg,
-                            tf.seg_ID: obj_id,
-                            tf.vis_det: 1,
-                            tf.FLUX_VIS_APER: 1E-10, #WHERE DOES THIS COME FROM?
-                            tf.SEGMENTATION_AREA: 1 #WHERE DOES THIS COME FROM?
-                            })
+    #create the table columns
+    object_ids = [i+1 for i in range (n_objs)]
+    ras = [c.ra.deg for c in obj_coords]
+    decs = [c.dec.deg for c in obj_coords]
+    vis_det = np.ones(n_objs, dtype=np.int16)
+    flux_vis_aper = np.ones(n_objs,dtype=np.float32)*1E-10
+    seg_area = np.ones(n_objs,dtype=np.int32)
     
+    #create the table
+    table = initialise_mer_final_catalog(init_cols={
+                                                    tf.ID: object_ids,
+                                                    tf.gal_x_world: ras,
+                                                    tf.gal_y_world: decs,
+                                                    tf.seg_ID: object_ids,
+                                                    tf.vis_det: vis_det,
+                                                    tf.FLUX_VIS_APER: flux_vis_aper,
+                                                    tf.SEGMENTATION_AREA: seg_area})
+
+
     #get a filename for the table
     table_filename = get_allowed_filename("MER-CAT", "00", version=ppt_version)
     qualified_table_filename = os.path.join(workdir, table_filename)
@@ -85,7 +93,7 @@ def create_catalogue(obj_coords=[], workdir="."):
     logger.info("Writing dpdMerFinalCatalog product to %s"%product_filename)
     write_xml_product(product, qualified_product_filename)
 
-    return product_filename
+    return product_filename, object_ids
 
     
 
