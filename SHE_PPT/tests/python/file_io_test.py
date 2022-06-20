9e762d20de1de8bee4fa3a32f0bb3e1dce32ebbd
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2022-06-20 15:34:48 +0100

    Adding test of `symlink_contents`

--- SHE_PPT/tests/python/file_io_test.py
+++ SHE_PPT/tests/python/file_io_test.py
@@ -19,7 +19,7 @@ __updated__ = "2021-08-20"
 #
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-
+import filecmp
 import os
 import shutil
 import stat
@@ -49,7 +49,8 @@ from SHE_PPT.file_io import (DATA_SUBDIR, DEFAULT_FILE_EXTENSION, DEFAULT_FILE_S
                              read_d_l_method_table_filenames, read_d_l_method_tables, read_d_method_table_filenames,
                              read_d_method_tables, read_fits, read_listfile, read_product_and_table, read_table,
                              read_table_from_product, read_xml_product, remove_files, replace_in_file,
-                             replace_multiple_in_file, safe_copy, tar_files, try_remove_file, type_name_maxlen,
+                             replace_multiple_in_file, safe_copy, symlink_contents, tar_files, try_remove_file,
+                             type_name_maxlen,
                              update_xml_with_value, write_fits, write_listfile, write_product_and_table, write_table,
                              write_xml_product, )
 from SHE_PPT.products.mer_final_catalog import create_dpd_mer_final_catalog
@@ -1110,6 +1111,58 @@ class TestIO(SheTestCase):
         os.remove(qualified_dest_listfile_filename)
         os.remove(qualified_dest_product_filename)
         os.remove(qualified_dest_table_filename)
+
+    def test_symlink_contents(self):
+        """ Unit test for SHE_PPT.file_io.symlink_contents function.
+        """
+
+        # Clean out the destination directory
+        shutil.rmtree(self.dest_dir)
+
+        # Symlink the contents of the source directory to the destination directory
+        symlink_contents(src_dir = self.src_dir,
+                         dest_dir = self.dest_dir)
+
+        # Check that the destination directory now contains the same files as the source directory
+
+        for filename in os.listdir(self.src_dir):
+
+            src_filename = os.path.join(self.src_dir, filename)
+            dest_filename = os.path.join(self.dest_dir, filename)
+
+            assert os.path.exists(dest_filename)
+
+            if os.path.isfile(src_filename):
+
+                # It's a file, so compare in the source and destination directory
+                assert filecmp.cmp(src_filename, dest_filename), f"{src_filename} != {dest_filename}"
+
+                # Check that the destination file is a symlink
+                assert os.path.islink(dest_filename)
+
+            else:
+
+                assert os.path.isdir(dest_filename)
+
+                # It's a directory, so compare the contents of the source and destination directory
+                for subfilename in os.listdir(src_filename):
+
+                    src_subfilename = os.path.join(src_filename, subfilename)
+                    dest_subfilename = os.path.join(dest_filename, subfilename)
+
+                    assert os.path.exists(dest_subfilename)
+
+                    if os.path.isfile(src_subfilename):
+
+                        assert filecmp.cmp(src_subfilename, dest_subfilename), \
+                            f"{src_subfilename} != {dest_subfilename}"
+                        assert os.path.islink(dest_subfilename)
+
+                    else:
+
+                        assert os.path.isdir(dest_subfilename)
+
+                        # Stop here if it's a directory; no need to test even deeper
 
     def test_try_remove_file(self):
         """Unit test of the `try_remove_file` function.
